396434be107fc90f534339d3ecb7e658ec0e4e86
btran369 <VuAnhQuan.Tran@georgebrown.ca>
2025-04-01 18:05:54 -0400

    changed monster.py to have spell weakness and resistance

--- monster.py
+++ monster.py
@@ -1,6 +1,7 @@
 import random
 from character import Character
 
+
 class Monster(Character):
     def __init__(self):
         # Explicitly call parent constructor with random values
@@ -8,7 +9,12 @@ class Monster(Character):
             combat_strength=random.randint(1, 12),
             health_points=random.randint(1, 20)
         )
-        print(f"A monster appears with {self.m_combat_strength} combat strength and {self.m_health_points} health points!")
+        spell_type = ["Fire", "Ice", "Lightning", "Earth", "Water"]
+        self.__spell_weakness = random.choice(spell_type)
+        spell_type.remove(self.__spell_weakness)
+        self.__spell_resistance = random.choice(spell_type)
+        spell_type.remove(self.__spell_resistance)
+        print(f"A monster appears with {self.m_combat_strength} combat strength and {self.m_health_points} health points! It is weak against {self.__spell_weakness} spells and resistant to {self.__spell_resistance} spells!")
     
     def __del__(self):
         # First print Monster-specific message
@@ -32,6 +38,21 @@ class Monster(Character):
     @m_health_points.setter
     def m_health_points(self, value):
         self.health_points = value
+
+    @property
+    def spell_weakness(self):
+        return self.__spell_weakness
+    @spell_weakness.setter
+    def spell_weakness(self, value):
+        self.__spell_weakness = value
+
+    @property
+    def spell_resistance(self):
+        return self.__spell_resistance
+    
+    @spell_resistance.setter
+    def spell_resistance(self, value):
+        self.__spell_resistance = value
     
     def monster_attacks(self):
         # Return the combat strength as the attack value
